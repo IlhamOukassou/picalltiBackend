0d2d5b1e7010cd51dfeebba710c7d363a18900b6
hervelinne <mimissa20@hotmail.fr>
2022-12-30 22:44:28 +0100

    adding pwd encoding and sign up verification

--- src/main/java/com/example/picallti/PicalltiApplication.java
+++ src/main/java/com/example/picallti/PicalltiApplication.java
@@ -7,17 +7,21 @@ import com.example.picallti.repository.CommentaireRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.crypto.Data;
 import java.sql.Time;
 import java.util.Date;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PicalltiApplication {
 	public static void main(String[] args) {
 
-		SpringApplication.run(PicalltiApplication.class, args);
+		SpringApplication. run(PicalltiApplication.class, args);
 	}
+
 
 
 }
--- src/main/java/com/example/picallti/service/UserService.java
+++ src/main/java/com/example/picallti/service/UserService.java
@@ -6,7 +6,12 @@ import com.example.picallti.exception.LoginRequestException;
 import com.example.picallti.model.User;
 import com.example.picallti.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -17,9 +22,25 @@ public class UserService {
     @Autowired
     public UserRepository userRepository;
 
-    public void addUser(User user){
-        userRepository.save(user);
+    BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+
+    public void addUser(User user) {
+        Optional<User> userByEmail = userRepository
+                .findByEmail(user.getEmail());
+        Optional<User> userByPhone = userRepository
+                .findByPhone(user.getPhone());
+        if(userByEmail.isPresent()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Email already exists !!");
+        }else if(userByPhone.isPresent()){
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Phone Number is already taken !!");
+        }else {
+            String passwordEncoded = bcrypt.encode(user.getPassword());
+            user.setPassword(passwordEncoded);
+            userRepository.save(user);
+        }
+
     }
+
 
     public Collection<User> getAllUsers(){
         return userRepository.findAll();
@@ -33,6 +54,10 @@ public class UserService {
         return userRepository.findByEmail(email);
     }
 
+    public Optional<User> getUserByPhone (int phone){
+        return userRepository.findByPhone(phone);
+    }
+
     public void update(User user){
         User userFromDb = userRepository.findById(user.getId()).get();
         if (userFromDb != null){
@@ -40,7 +65,8 @@ public class UserService {
             userFromDb.setNom(user.getNom());
             userFromDb.setGenre(user.getGenre());
             userFromDb.setEmail(user.getEmail());
-            userFromDb.setPassword(user.getPassword());
+            String passwordEncoded = bcrypt.encode(userFromDb.getPassword());
+            userFromDb.setPassword(passwordEncoded);
             userFromDb.setPhone(user.getPhone());
             userFromDb.setPhoto(user.getPhoto());
             userFromDb.setBio(user.getBio());
@@ -54,8 +80,14 @@ public class UserService {
     }
 
     public Optional<User> loginUser(LoginRequestDTO loginRequestDTO){
-        return Optional.ofNullable(userRepository.findOneByEmailIgnoreCaseAndPassword(loginRequestDTO.getEmail(), loginRequestDTO.getPassword())
-                .orElseThrow(() -> new LoginRequestException("Login Error !")));
+        Optional<User> opUser = userRepository.findByEmail(loginRequestDTO.getEmail());
+        if(opUser.isPresent()){
+            User dbUser = opUser.get(); 
+            if(bcrypt.matches(loginRequestDTO.getPassword(),dbUser.getPassword())){
+                return opUser; 
+            }else throw new LoginRequestException("Password is wrong");
+        }else throw new LoginRequestException("Email is wrong !"); 
+
     }
 
 
